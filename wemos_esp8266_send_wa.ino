5c6b05a3e19b66c498187084388b8be3a68729e1
barru kurniawan <32356232+barrukurniawan@users.noreply.github.com>
2020-04-19 11:16:52 +0700

    Update wemos_esp8266_send_wa.ino

--- wemos_esp8266_send_wa.ino
+++ wemos_esp8266_send_wa.ino
@@ -1,50 +1,77 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include "base64.h"
 
-const char* ssid = "xxxxxxxxxx";
-const char* password = "xxxxxxxxxxxx";
+// Setup WiFi network
+const char* ssid = "xxxxxxxxxxxx";
+const char* password = "xxxxxxxxxx";
 
-// sensor parameter
+// read sensor
 const int sensorPin = A0;
-int state = 0;
 int speakerPin = D5;
+int state = 0;
 
-void setup () {
+// Twilio Parameter
+String account_sid = "Axxxxxxxxxxxxxxxxxxxxxxxxxx";
+String auth_token = "8xxxxxxxxxxxxxxxxxxxxxxxxxx";
+String from = "14xxxxxxxxx";
+String to = "62xxxxxxxxxxxxxxx";
+String body = "Ada penyusup masuk kerumah mu!";
+
+void setup() {
  
   Serial.begin(115200);
+ 
   WiFi.begin(ssid, password);
-
+  
   pinMode(sensorPin,INPUT);
   pinMode(speakerPin, OUTPUT);
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.print("Connecting..");
+    Serial.println("Connecting to WiFi..");
   }
-
+ 
+  Serial.println("Connected to WiFi");
+ 
 }
  
 void loop() {
+
   state = analogRead(sensorPin);
+//  Serial.println(state);
   if(state < 500) {
-      if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
-       
-      HTTPClient http;  //Declare an object of class HTTPClient
-      String value = (String) state;
-      http.begin("http://jagorobot.pythonanywhere.com/kirimwa?nilai=" + value);  // send POST WA notification
-      int httpCode = http.GET();   
-       
-      if (httpCode > 0) {
-        String payload = http.getString();  
-        Serial.println(payload);     
+      
+      if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
+     
+        HTTPClient http;
+     
+        int nilai = random(29,37);
+        String data = (String) nilai;
+//        String link = "http://jagorobot.pythonanywhere.com/kirimwa?nilai=" + state;
+        String link = "http://jagorobot.pythonanywhere.com/kirimwa?account_sid=" + account_sid + "&auth_token="+ auth_token +"&to_wa=" + to + "&from_wa="+ from +"&body_message=" + body;
+        
+        http.begin(link);
+        int httpCode = http.GET();
+        Serial.println(httpCode);
+        
+        if (httpCode > 0) { //Check for the returning code
+            
+            String payload = http.getString();
+            Serial.println(link);
+            Serial.println(httpCode);
+            Serial.println(payload);
+        }
+        else {
+            Serial.println("Error on HTTP request");
+        }
+        http.end();
       }
-        http.end();   //Close connection
-      }
-      // turn on alarm
       digitalWrite(speakerPin, HIGH);
       delay(3000);
       digitalWrite(speakerPin, LOW);
       delay(1000);
   }
-  delay(500);
+  delay(200);
+ 
 }
